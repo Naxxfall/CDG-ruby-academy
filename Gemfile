f67e52f95b75270a678d403d2454e9779303bb86
Sergey Fedorchenko <fed0r492@yandex.ru>
2022-06-26 21:30:38 +0300

    Added shrine gem

--- Gemfile
+++ Gemfile
@@ -50,7 +50,8 @@ gem "bootsnap", require: false
 # gem "image_processing", "~> 1.2"
 #
 gem "pundit"
-gem 'devise'
+gem "devise"
+gem "shrine", "~> 3.0"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_20_075501) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_26_180254) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_20_075501) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "image_data"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
